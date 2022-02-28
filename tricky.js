8cee0a3c93d2e8c89e111bd88dfd22835b14837b
Mustafizur Rahman <mustafizur232@gmail.com>
2022-03-01 02:19:52 +0600

    twentythree

--- tricky.js
+++ tricky.js
@@ -3,12 +3,42 @@ const friends = {
     age: 34,
     color: 'black',
 }
+
+for (const prop in friends) {
+    // console.log(prop, friends[prop]);
+}
 const keys = Object.keys(friends);
-console.log(keys);
-const values = Object.values(friends);
-console.log(values);
-// Object.seal(friends);
-friends.occupation = "student";
-friends.age = 40;
-// delete friends.color;
-console.log(friends);
+// console.log(keys);
+for (const peop of keys) {
+    console.log(peop, friends[peop]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const keys = Object.keys(friends);
+// console.log(keys);
+// const values = Object.values(friends);
+// console.log(values);
+// // Object.seal(friends);
+// friends.occupation = "student";
+// friends.age = 40;
+// // delete friends.color;
+// console.log(friends);
